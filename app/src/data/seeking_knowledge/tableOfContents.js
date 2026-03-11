7c18e80994c674f24405515fdd0f7fe990eb90a3
SyedMas8d <syedmasood.s@foodhub.com>
2026-03-11 07:29:30 +0530

    Update | Evidence Of Author of the quran

--- app/src/data/seeking_knowledge/tableOfContents.js
+++ app/src/data/seeking_knowledge/tableOfContents.js
@@ -3,28 +3,86 @@ export const knowledgeData = {
         title: "Who is author of the qur'an",
         data: [
             {
-                title: 'Episode 1',
+                title: 'Evidence of prophecy about Pharaoh',
                 youtube_link: 'https://www.youtube.com/watch?v=BJ_4DlLFT2Y&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_',
             },
             {
-                title: 'Episode 2',
+                title: 'Evidence of prophecy about Abu Lahab',
                 youtube_link:
                     'https://www.youtube.com/watch?v=PIVCyuSLNHQ&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=2',
             },
             {
-                title: 'Episode 3',
+                title: 'Evidence of prophecy about Raising Rank Of Prophet Muhammad (صَلَّى ٱللَّٰهُ عَلَيْهِ وَسَلَّمَ)',
                 youtube_link:
                     'https://www.youtube.com/watch?v=geDohFMHm08&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=3',
             },
             {
-                title: 'Episode 4',
-                youtube_link:
-                    'https://www.youtube.com/watch?v=StvfXeJ_l4A&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=4',
-            },
-            {
-                title: 'Episode 5',
+                title: 'Evidence of prophecy about The People of the Cave (Ashabul Kahf)',
                 youtube_link:
                     'https://www.youtube.com/watch?v=i5iNnPra830&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=5',
+            },
+            {
+                title: 'Nuggests of knowledge about Jesus & Adam',
+                youtube_link:
+                    'https://www.youtube.com/watch?v=VNIuZP_6csA&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=8',
+            },
+            {
+                title: 'Nuggests of knowledge Al-Baqara',
+                youtube_link:
+                    'https://www.youtube.com/watch?v=qBqcqIqw3H0&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=7',
+            },
+            {
+                title: 'Nuggests of knowledge Chapter The People of the Cave',
+                youtube_link:
+                    'https://www.youtube.com/watch?v=QA8bj_PLZwM&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=9',
+            },
+            {
+                title: "Criticism about Prophet Muhammad (صَلَّى ٱللَّٰهُ عَلَيْهِ وَسَلَّمَ) &  Eloquent of the qur'an",
+                youtube_link:
+                    'https://www.youtube.com/watch?v=rE79icdJJvE&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=10',
+            },
+            {
+                title: 'Correctness of history about Solomon (Sulaiman عَلَيْهِ ٱلسَّلَامُ) $ Hixos',
+                youtube_link:
+                    'https://www.youtube.com/watch?v=0KpXPUzv-lI&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=11',
+            },
+            {
+                title: "Evidence of prophecy about Preserving of the qur'an",
+                youtube_link:
+                    'https://www.youtube.com/watch?v=qazzUhSmk2s&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=12',
+            },
+            {
+                title: "Eloquent of messages (The Qur'an, Hadith & Divine Hadith)",
+                youtube_link:
+                    'https://www.youtube.com/watch?v=lPtavajeJ94&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=13',
+            },
+            {
+                title: 'Evidence of mountains',
+                youtube_link:
+                    'https://www.youtube.com/watch?v=FiIlYCr2TR4&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=14',
+            },
+            {
+                title: 'Evidence of gender classification about ants & bees',
+                youtube_link:
+                    'https://www.youtube.com/watch?v=ijDEBZLu0ec&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=15',
+            },
+            {
+                title: "Open challenge of the Qur'an",
+                youtube_link:
+                    'https://www.youtube.com/watch?v=Nc8fNFMMNm8&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=16',
+            },
+            {
+                title: 'Laws without conLaws without contradiction at one try',
+                youtube_link:
+                    'https://www.youtube.com/watch?v=vaLqeexknOs&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=17',
+            },
+            {
+                title: 'Nuggests of knowledge about feminine',
+                youtube_link:
+                    'https://www.youtube.com/watch?v=gts8c0Mq7hc&list=PLwM5TcqtLM4g9-iUtTN4n76jwH2xXohL_&index=18',
+            },
+            {
+                title: 'Mention of Moses  (Musa عَلَيْهِ ٱلسَّلَامُ)',
             },
         ],
     },
